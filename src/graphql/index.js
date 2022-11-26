ffcaf4ea1f011d80b5b5434345eb5259e5b3203e
dharmveer97 <dvbangar8@gmail.com>
2022-11-26 21:16:21 +0530

    clean code

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,5 +1,12 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
-import { typeDefs, resolvers } from './mergeTools';
+import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+import MainSchema from './main/schema';
+import MainResolvers from './main/resolvers';
+
+export const typeDefs = mergeTypeDefs([MainSchema]);
+export const resolvers = mergeResolvers([
+  MainResolvers,
+]);
 
 const graphqlSchema = makeExecutableSchema({
   typeDefs,
--- src/graphql/mergeTools.js
+++
@@ -1,9 +0,0 @@
-import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
-
-import MainSchema from './main/schema';
-import MainResolvers from './main/resolvers';
-
-export const typeDefs = mergeTypeDefs([MainSchema]);
-export const resolvers = mergeResolvers([
-  MainResolvers,
-]);
