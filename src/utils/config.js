1040b0ff54893167499a56a2b40f2e29c6a82935
dharmveer97 <dvbangar8@gmail.com>
2022-11-26 21:10:22 +0530

    fixed

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export default {
   debug: process.env.NODE_ENV === 'development',
-  siteName: 'kickof',
+  siteName: 'kickoff-next-js',
   siteUrl: 'https://www.test.co.uk',
   instagram: 'https://www.instagram.com/my-site',
   facebook: 'https://www.facebook.com/my-site',
